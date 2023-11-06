5ee24c0a6ac41afe8be6a9c4187dcd6bab5d6ab8
Aduragbemi <hardupet@gmail.com>
2023-11-06 12:53:52 +0000

    Payment Module and Docker file addition

--- obj/Debug/net7.0/FinanceManagementSystem.AssemblyInfo.cs
+++ obj/Debug/net7.0/FinanceManagementSystem.AssemblyInfo.cs
@@ -12,6 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: Microsoft.AspNetCore.Identity.UI.UIFrameworkAttribute("Bootstrap5")]
+[assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("273ef4c4-f9d2-46cc-9216-efb678074924")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("FinanceManagementSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
